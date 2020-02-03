d44e2c99437e0fa6b7d2ab8b691c1023255f6859
nish <nish@nishdev>
2020-02-03 11:26:46 +0000

    update to new test run

---
+++ src/model/db.py
@@ -0,0 +1,22 @@
+'''
+Created on 3 Feb 2020
+
+@author: nish
+'''
+
+from sqlalchemy import engine
+
+class ConnStringFactory:
+    
+    @staticmethod
+    def build_connection_string(product=None):
+        pass
+    
+
+class DatabaseManager:
+    
+    def __init__(self):
+        pass
+
+class
+        
